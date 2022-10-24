eb6a20012f61e1056391ceaaeed6b031ac02f123
drewlearns <drewlearns@hey.com>
2022-10-24 07:09:36 -0400

    merge conflict

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'os_100days' });
+router.get("/", function (req, res, next) {
+	res.render("index", { title: "os_100days" });
 });
-router.get('/faq', function(req, res, next) {
-  res.render('faq', { title: 'faq' });
+router.get("/faq", function (req, res, next) {
+	res.render("faq", { title: "faq" });
 });
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'about' });
+router.get("/about", function (req, res, next) {
+	res.render("about", { title: "about" });
+});
+router.get("/signup", function (req, res, next) {
+	res.render("signup", { title: "signup" });
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('sign in', { title: 'sign in' });
 });
 
 module.exports = router;
